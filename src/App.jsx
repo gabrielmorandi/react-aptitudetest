3035eb0f50d460be566e1fb0ae66dd6d18e79863
Gabriel Morandi de Mello <61758357+gabrielmorandi@users.noreply.github.com>
2022-04-12 05:56:22 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './componentes/header/Header'
+import Nav from './componentes/nav/Nav.jsx'
+
+const App = () => {
+  return (
+    <>
+        <Header />
+        <Nav />
+    </>
+  )
+}
+
+export default App
